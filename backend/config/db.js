6cfdfad9e6a515e5c635df216dbcb208e2771e53
Ifrat <ifrat1998@gmail.com>
2025-08-05 14:01:57 +0500

    smart

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: 'postgresql://efrat:yxAxdRUuv5Bq9zWFGmqI80Y8zwvr9dcL@dpg-d28ft7qdbo4c73fi1c8g-a.oregon-postgres.render.com/fermerdb',
+  ssl: {rejectUnauthorized: false}
 });
 
 module.exports = pool;
