7a71fb529bc035167e0278330177eedef4d5549b
Ifrat <ifrat1998@gmail.com>
2025-08-07 08:03:57 +0500

    smart

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -36,7 +36,7 @@ export default function Footer() {
             <FaInstagram className="text-lg" />
           </a>
           <a 
-            href="https://wa.me/77001234567" 
+            href="https://wa.me/77051439373?text=%D0%A1%D3%99%D0%BB%D0%B5%D0%BC!%20SmartFarmer%20%D0%B6%D0%BE%D0%B1%D0%B0%D1%81%D1%8B%20%D0%B6%D0%B0%D0%B9%D0%BB%D1%8B%20%D0%B6%D0%B0%D0%B7%D1%8B%D0%BF%20%D0%BE%D1%82%D1%8B%D1%80%D0%BC%D1%8B%D0%BD.%20%D0%91%D2%B1%D0%BB%20%D1%84%D0%B5%D1%80%D0%BC%D0%B5%D1%80%D0%BB%D0%B5%D1%80%D0%B3%D0%B5%20%D0%B0%D1%80%D0%BD%D0%B0%D0%BB%D2%93%D0%B0%D0%BD%20%D1%86%D0%B8%D1%84%D1%80%D0%BB%D1%8B%D2%9B%20%D0%BA%D3%A9%D0%BC%D0%B5%D0%BA%D1%88%D1%96%20%E2%80%93%20%D0%B0%D1%83%D0%B0%20%D1%80%D0%B0%D0%B9%D1%8B,%20%D0%BD%D0%B0%D1%80%D1%8B%D2%9B%20%D0%B1%D0%B0%D2%93%D0%B0%D0%BB%D0%B0%D1%80%D1%8B,%20%D0%BA%D0%B5%D2%A3%D0%B5%D1%81%D1%82%D0%B5%D1%80%20%D0%B6%D3%99%D0%BD%D0%B5%20%D1%82%D0%B0%D2%93%D1%8B%20%D0%B1%D0%B0%D1%81%D2%9B%D0%B0%20%D0%BF%D0%B0%D0%B9%D0%B4%D0%B0%D0%BB%D1%8B%20%D0%B0%D2%9B%D0%BF%D0%B0%D1%80%D0%B0%D1%82%D1%82%D0%B0%D1%80%20%D0%B1%D0%B0%D1%80.%20%D2%9A%D1%8B%D0%B7%D1%8B%D2%9B%D1%82%D1%8B%D1%80%D1%81%D0%B0,%20%D1%82%D0%BE%D0%BB%D1%8B%D2%93%D1%8B%D1%80%D0%B0%D2%9B%20%D0%B0%D0%B9%D1%82%D1%8B%D0%BF%20%D0%B1%D0%B5%D1%80%D0%B5%20%D0%B0%D0%BB%D0%B0%D0%BC%D1%8B%D0%BD!" 
             target="_blank" 
             rel="noopener noreferrer" 
             className="bg-green-700 hover:bg-green-600 p-3 rounded-full text-white transition-all duration-300 transform hover:scale-110 shadow-md"
